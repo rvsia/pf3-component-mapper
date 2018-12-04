fd4a6dfd3beaa462985f57f9c8c0c00314e88305
Richard Vsiansky <r.vsia@seznam.cz>
2018-12-04 14:30:28 +0100

    feat(switch): add switch component

--- src/form-fields/component-mapper.js
+++ src/form-fields/component-mapper.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { componentTypes } from '@data-driven-forms/react-form-renderer';
-import { TextField, TextareaField, SelectField, CheckboxGroup, Radio } from './form-fields';
+import { TextField, TextareaField, SelectField, CheckboxGroup, Radio, SwitchField } from './form-fields';
 import SubForm from './sub-form';
 import Tabs from './tabs';
 
@@ -15,6 +15,7 @@ const mapper = {
   [componentTypes.DATE_PICKER]: props => <div>date picker</div>,
   [componentTypes.TIME_PICKER]: props => <div>time picker</div>,
   [componentTypes.TAG_CONTROL]: props => <div>tag control</div>,
+  [componentTypes.SWITCH]: SwitchField,
 };
 
 export default mapper;
